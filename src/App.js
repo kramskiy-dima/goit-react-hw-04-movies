fc08402f62c758907a27c75238f34a48bc154993
Kramskiy Dima <kramskiyd@gmail.com>
2021-03-24 22:07:20 +0200

    adds Route, Switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
-function App() {
-  return <div className="App"></div>;
+import React, { Component, lazy, Suspense } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import routes from './routes';
+import AppBar from './components/AppBar';
+import Container from './components/Container';
+
+const HomeView = lazy(() =>
+  import('./views/HomeView' /* webpackChunkName: "HomeView" */),
+);
+const MovieView = lazy(() =>
+  import('./views/MovieView' /* webpackChunkName: "MovieView" */),
+);
+const MovieDetailsPage = lazy(() =>
+  import('./views/MovieDetailsPage' /* webpackChunkName: "MovieDetailsPage" */),
+);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Container>
+          <AppBar />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route exact path={routes.home} component={HomeView} />
+              <Route path={routes.movie} component={MovieDetailsPage} />
+              <Route path={routes.movies} component={MovieView} />
+              <Route component={HomeView} />
+            </Switch>
+          </Suspense>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
